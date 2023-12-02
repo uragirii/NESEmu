e9846f68940020413d0d54d65ef238d1cc4fe5f3
Apoorv Kansal <apoorvkansalak@gmail.com>
2023-12-02 20:01:35 +0530

    added lda, ldx, sta

--- src/6502.ts
+++ src/6502.ts
@@ -1,4 +1,11 @@
-import { DEBUG_LOC, MEMORY_SIZE } from "./constants";
+import {
+  ADDRESSING_C_01,
+  ADDRESSING_C_10,
+  CYCLES_PER_SECOND,
+  DEBUG_LOC,
+  MEMORY_SIZE,
+} from "./constants";
+import { AddressingMode } from "./types";
 
 export class Mos6502 {
   memory = new Uint8Array(MEMORY_SIZE);
@@ -48,6 +55,8 @@ export class Mos6502 {
     this._stackPointer[0] = val;
   }
 
+  private _cycles = 0;
+
   constructor(buffer: ArrayBuffer, startPos?: number) {
     if (buffer.byteLength > MEMORY_SIZE) {
       throw new Error(
@@ -76,21 +85,252 @@ export class Mos6502 {
 
   public dumpRegisters = () => {
     console.log("-------DUMPING REGISTERS-------");
-    console.log(`Acc : ${this.acc.toString(16)}, ${this.acc}`);
-    console.log(`X : ${this.x.toString(16)}, ${this.x}`);
-    console.log(`Y : ${this.y.toString(16)}, ${this.y}`);
-    console.log(`SP : ${this.stackPointer.toString(16)}, ${this.stackPointer}`);
+    console.log(`Acc : 0x${this.acc.toString(16)}, ${this.acc}`);
+    console.log(`X : 0x${this.x.toString(16)}, ${this.x}`);
+    console.log(`Y : 0x${this.y.toString(16)}, ${this.y}`);
     console.log(
-      `PC : ${this.programCounter.toString(16)}, ${this.programCounter}`
+      `SP : 0x${this.stackPointer.toString(16)}, ${this.stackPointer}`
     );
     console.log(
-      `Status : ${this.statusReg.toString(2).padStart(8, "0")}, ${
+      `PC : 0x${this.programCounter.toString(16)}, ${this.programCounter}`
+    );
+    console.log(
+      `Status : 0b${this.statusReg.toString(2).padStart(8, "0")}, ${
         this.statusReg
       }`
     );
   };
 
+  private emuCycle = async (cycles: number) => {
+    this._cycles += cycles;
+
+    const time = (this._cycles / CYCLES_PER_SECOND) * 1000;
+
+    if (time > Math.random() * 50) {
+      return new Promise((resolve) =>
+        setTimeout(() => {
+          this._cycles = 0;
+          resolve(undefined);
+        }, time)
+      );
+    }
+    return Promise.resolve(undefined);
+  };
+
+  private fetch2Bytes = () => {
+    const lb = this.fetchOpcode();
+    const hb = this.fetchOpcode();
+    return (hb << 8) | lb;
+  };
+
+  private read2Bytes = (address: number) => {
+    const lb = this.memory[address];
+    const hb = this.memory[address + 1];
+    return (hb << 8) | lb;
+  };
+
+  private getAddressing = async (
+    mode: AddressingMode
+  ): Promise<{ address: null | number; value: null | number }> => {
+    switch (mode) {
+      case "immediate": {
+        return { value: this.fetchOpcode(), address: null };
+      }
+      case "zpg": {
+        const address = this.fetchOpcode();
+        return {
+          address,
+          value: this.memory[address],
+        };
+      }
+      case "accumulator": {
+        return {
+          value: this.acc,
+          address: null,
+        };
+      }
+      case "absolute": {
+        return { value: null, address: this.fetch2Bytes() };
+      }
+      case "zpg-x": {
+        const lb = this.fetchOpcode();
+
+        const address = this.x + lb;
+        return {
+          address,
+          value: this.memory[address],
+        };
+      }
+      case "zpg-y": {
+        const lb = this.fetchOpcode();
+
+        const address = this.y + lb;
+        return {
+          address,
+          value: this.memory[address],
+        };
+      }
+      case "abs-x": {
+        const address = this.fetch2Bytes() + this.x;
+        return {
+          address,
+          value: null,
+        };
+      }
+      case "abs-y": {
+        const address = this.fetch2Bytes() + this.y;
+        return {
+          address,
+          value: null,
+        };
+      }
+      default: {
+        throw new Error(`Addressing mode ${mode} not implemented`);
+      }
+    }
+  };
+
   private executeOpcode = async (opcode: number) => {
-    throw new Error(`unknown opcode ${opcode.toString(16)}`);
+    switch (opcode) {
+      case 0xd8: {
+        // cld
+        // Clear Decimal Mode
+        this.statusReg = this.statusReg & 0b01;
+        await this.emuCycle(2);
+        break;
+      }
+      case 0x8a: {
+        // txa
+        // x -> a
+        this.acc = this.x;
+        await this.emuCycle(2);
+        break;
+      }
+      case 0x9a: {
+        // txs
+        // X -> SP
+        this.stackPointer = this.x;
+        await this.emuCycle(2);
+        break;
+      }
+      case 0xaa: {
+        // tax
+        // a -> x
+        this.x = this.acc;
+        await this.emuCycle(2);
+        break;
+      }
+      case 0xba: {
+        // tsx
+        // sp -> x
+        this.x = this.stackPointer;
+        await this.emuCycle(2);
+        break;
+      }
+      case 0xca: {
+        // dex;
+        this.x--;
+        await this.emuCycle(2);
+        break;
+      }
+      case 0xea: {
+        // nop
+        await this.emuCycle(2);
+        break;
+      }
+      case 0x4c: {
+        // jmp
+        // abs
+        const address = this.fetch2Bytes();
+        this.programCounter = address;
+        await this.emuCycle(3);
+        break;
+      }
+      case 0x6c: {
+        // jmp
+        // indirect
+        const value = this.fetch2Bytes();
+        const address = this.read2Bytes(value);
+        this.programCounter = address;
+        await this.emuCycle(5);
+        break;
+      }
+      default: {
+        // opcode is aaabbbcc
+        const cc = opcode & 0b11;
+        const bbb = (opcode & 0b11100) >> 2;
+        const aaa = opcode >> 5;
+
+        switch (cc) {
+          case 0b01: {
+            // group 1
+            const mode = ADDRESSING_C_01[bbb];
+            switch (aaa) {
+              case 0b100: {
+                // sta
+                // A -> M;
+                const { address } = await this.getAddressing(mode);
+                if (address === null) {
+                  throw new Error(
+                    `sta incorrect no address, ${opcode.toString(
+                      16
+                    )} mode:${mode}`
+                  );
+                }
+                this.memory[address] = this.acc;
+                await this.emuCycle(5);
+                break;
+              }
+              case 0b101: {
+                // lda
+                const { value } = await this.getAddressing(mode);
+                if (value === null) {
+                  throw new Error(
+                    `lda incorrect no value, ${opcode.toString(
+                      16
+                    )} mode:${mode}`
+                  );
+                }
+                this.acc = value;
+                await this.emuCycle(4);
+                break;
+              }
+              default: {
+                throw new Error(`unknown opcode ${opcode.toString(16)}`);
+              }
+            }
+            break;
+          }
+          case 0b10: {
+            // group 2
+
+            switch (aaa) {
+              case 0b101: {
+                //ldx
+                const mode = ADDRESSING_C_10[bbb];
+                const { value } = await this.getAddressing(
+                  mode.replace("x", "y") as AddressingMode
+                );
+                if (value === null) {
+                  throw new Error(
+                    `ldx incorrect no value, ${opcode.toString(16)}`
+                  );
+                }
+                this.x = value;
+                await this.emuCycle(4);
+                break;
+              }
+              default: {
+                throw new Error(`unknown opcode ${opcode.toString(16)}`);
+              }
+            }
+            break;
+          }
+          default: {
+            throw new Error(`unknown opcode ${opcode.toString(16)}`);
+          }
+        }
+      }
+    }
   };
 }
--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,43 @@
+import { AddressingMode } from "./types";
+
 export const MEMORY_SIZE = 2 ** 16;
 // 1.79Mhz
 export const CYCLES_PER_SECOND = 1790000;
 
 export const DEBUG_LOC: null | number = null;
+
+// 000	#immediate
+// 001	zero page
+// 010	accumulator
+// 011	absolute
+// 101	zero page,X
+// 111	absolute,X
+
+export const ADDRESSING_C_10: Record<number, AddressingMode> = {
+  0b000: "immediate",
+  0b001: "zpg",
+  0b010: "accumulator",
+  0b011: "absolute",
+  0b101: "zpg-x",
+  0b111: "abs-x",
+};
+
+// 000	(zero page,X)
+// 001	zero page
+// 010	#immediate
+// 011	absolute
+// 100	(zero page),Y
+// 101	zero page,X
+// 110	absolute,Y
+// 111	absolute,X
+
+export const ADDRESSING_C_01: Record<number, AddressingMode> = {
+  0b000: "x-indirect",
+  0b001: "zpg",
+  0b010: "immediate",
+  0b011: "absolute",
+  0b100: "indirect-y",
+  0b101: "zpg-x",
+  0b110: "abs-y",
+  0b111: "abs-x",
+};
