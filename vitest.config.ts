ac1caf08575ecc0f64faa2792787f7c270af87ef
Apoorv Kansal <apoorvkansalak@gmail.com>
2023-12-22 20:32:56 +0530

    fix: fix build path

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,18 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   test: {
     setupFiles: "./src/tests/setup.ts",
     testTimeout: 10000,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "/index.html"),
+        ppu: resolve(__dirname, "/ppu.html"),
+      },
+    },
   },
 });
